45c94a4f4970981a06291c8a867efebb855ae114
Sheema-parveen <sheemaparveenfeb14@gmail.com>
2024-06-27 08:52:51 +0530

    values send to webelements are stored here

---
+++ Test_Data/data.py
@@ -0,0 +1,9 @@
+class Data():
+    fname = "Sheema"
+    lname= "parveen"
+    email= "sheema@gmail.com"
+    number_inputbox = "9876543210"
+    subject_inputbox = "filling form"
+    pic = "C:/Users/USER/Desktop/workspace/demoqa/sample.png"
+    current_address = 'no:5, gandhi nagar, guindy , chennai'
+
